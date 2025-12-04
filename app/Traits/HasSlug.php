c75e92c34fb531678c0f6d39b931676659d84173
kennwat <slevin.lucky@ukr.net>
2025-12-04 14:55:57 +0000

    feat: add HasSlug trait and toggle visibility route

---
+++ app/Traits/HasSlug.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Support\Str;
+
+trait HasSlug
+{
+    public static function generateUniqueSlug(string $title): string
+    {
+        $slug = Str::slug($title);
+        $originalSlug = $slug;
+        $counter = 1;
+
+        while (static::where('slug', $slug)->exists()) {
+            $slug = $originalSlug.'-'.$counter;
+            $counter++;
+        }
+
+        return $slug;
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
-use App\Http\Controllers\Api\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\NewsController;
+use App\Http\Controllers\Api\ProfileController;
 
 // Auth
 Route::post('/register', [AuthController::class, 'register'])->name('register');
@@ -14,4 +15,10 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
     Route::match(['put', 'patch'], '/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    Route::apiResource('news', NewsController::class)->parameters(['news' => 'news:slug']);
+    
+    // toggle visibility of news
+    Route::patch('news/{news:slug}/toggle-visibility', [NewsController::class, 'toggleVisibility'])
+        ->name('news.toggle-visibility');
 });
