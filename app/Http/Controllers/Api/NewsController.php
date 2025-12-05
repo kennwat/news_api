7bbad2100d2e6dec07ef6ebae1018207428d5f69
kennwat <slevin.lucky@ukr.net>
2025-12-05 08:42:47 +0000

    feat: refactor search and visibility filters in index

--- app/Http/Controllers/Api/NewsController.php
+++ app/Http/Controllers/Api/NewsController.php
@@ -2,11 +2,11 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Http\Controllers\Controller;
+use App\Http\Resources\News\NewsListResource;
+use App\Http\Traits\CanLoadRelationships;
 use App\Models\News;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
-use App\Http\Traits\CanLoadRelationships;
-use App\Http\Resources\News\NewsListResource;
 
 class NewsController extends Controller
 {
@@ -22,6 +22,22 @@ class NewsController extends Controller
         // Dynamic loading of relationships via ?include=author,contentBlocks.details
         $query = $this->loadRelationships($query);
 
+        $query
+            ->when($request->input('search'), function ($q) use ($request) {
+                $search = $request->search;
+                $q->where(function ($query) use ($search) {
+                    $query->whereLike('title->de', "%{$search}%")
+                        ->orWhereLike('title->en', "%{$search}%")
+                        ->orWhereLike('short_description->de', "%{$search}%")
+                        ->orWhereLike('short_description->en', "%{$search}%");
+                });
+            })
+            ->when($request->filled('is_visible'), function ($q) use ($request) {
+                $q->where('is_visible', $request->boolean('is_visible'));
+            })
+            ->orderBy('created_at', 'desc');
+
+        // Pagination
         $perPage = $request->input('per_page', 10);
         $news = $query->paginate($perPage);
 
